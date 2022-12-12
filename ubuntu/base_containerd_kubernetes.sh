34f2d8a6ab0d8614cd705e77b76542d1b4de63c3
goflutterjava <goflutterjava@gmail.com>
2022-12-12 21:12:47 +0800

    add ovn script

--- ubuntu/base_containerd_kubernetes.sh
+++ ubuntu/base_containerd_kubernetes.sh
@@ -2,11 +2,18 @@
 
 DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y containerd
 sudo apt-get install -y apt-transport-https ca-certificates curl
+# docker image registry
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+# kubernetes image
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
+sudo apt-get install -y containerd.io
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
@@ -23,13 +30,14 @@ echo "net.bridge.bridge-nf-call-ip6tables=1" >> /etc/sysctl.d/kubernetes.conf
 echo "net.bridge.bridge-nf-call-iptables=1" >> /etc/sysctl.d/kubernetes.conf
 echo "net.ipv4.ip_forward=1" >> /etc/sysctl.d/kubernetes.conf
 sudo sysctl --system
-# pre download kubernetes images
-kubeadm config images pull
 mkdir -p /etc/containerd
 containerd config default > /etc/containerd/config.toml
-# modify /etc/containerd/config.toml sandbox_image to kubeadm config images
 systemctl restart containerd
+# pre download kubernetes images
+kubeadm config images pull
 wget -q https://github.com/Shoothzj/vm-dashboard/releases/download/latest/vm-dashboard.zip
+unzip vm-dashboard.zip
 mv dist /opt/vm-dashboard
 rm -rf vm-dashboard.zip
 cp $DIR/vm-dashboard.service /etc/systemd/system/vm-dashboard.service
+# manual modify /etc/containerd/config.toml sandbox_image to kubeadm config images
---
+++ ubuntu/base_containerd_kubernetes_ovn.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+wget -q https://raw.githubusercontent.com/kubeovn/kube-ovn/master/dist/images/install.sh
