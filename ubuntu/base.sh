4325f1108f873bc71d5f9e7dc7267cd21f626566
goflutterjava <goflutterjava@gmail.com>
2022-12-18 16:11:43 +0800

    optimize scripts

--- ubuntu/base.sh
+++ ubuntu/base.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y bridge-utils
+# c language
 sudo apt-get install -y cmake
 sudo apt-get install -y clang
+# ebpf
 sudo apt-get install -y linux-tools-$(uname -r) linux-headers-$(uname -r)
 sudo apt-get install -y perf-tools-unstable
 sudo apt-get install -y libbpfcc
 sudo apt-get install -y bpftrace
+# network cli
+sudo apt-get install -y bridge-utils arping net-tools
+sudo apt-get install -y ipvsadm
 sudo apt-get install -y iperf3
 sudo apt-get install -y qperf
-sudo apt-get install -y ipvsadm
+# openvswitch
+sudo apt-get install -y openvswitch-switch
+# helpful command
 echo "alias ..='cd ..'" >> /etc/bash.bashrc
 echo "alias ...='cd ../..'" >> /etc/bash.bashrc
 echo "alias tailf='tail -f'" >> /etc/bash.bashrc
 echo "set nu" >> /etc/vim/vimrc
+# java
 sudo apt-get install -y openjdk-17-jdk
 wget https://github.com/alibaba/arthas/releases/download/arthas-all-3.6.6/arthas-bin.zip
 mkdir -p /usr/local/lib/arthas
--- ubuntu/base_containerd_kubernetes_ovn.sh
+++ ubuntu/base_containerd_kubernetes_ovn.sh
@@ -2,3 +2,4 @@
 
 export DEBIAN_FRONTEND=noninteractive
 wget -q https://raw.githubusercontent.com/kubeovn/kube-ovn/master/dist/images/install.sh
+ctr -n k8s.io images pull kubeovn/kube-ovn:v1.11.0
