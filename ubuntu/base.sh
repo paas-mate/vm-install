b1f3cf8a0c8551f9524f0c9bd07d046d37273bf2
goflutterjava <goflutterjava@gmail.com>
2022-12-27 20:43:17 +0800

    Bump arthas from 3.6.6 to 3.6.7

--- ubuntu/base.sh
+++ ubuntu/base.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y cmake
 sudo apt-get install -y clang
 # ebpf
 sudo apt-get install -y linux-tools-$(uname -r) linux-headers-$(uname -r)
-sudo apt-get install -y perf-tools-unstable
+# sudo apt-get install -y perf-tools-unstable
 sudo apt-get install -y libbpfcc
 sudo apt-get install -y bpftrace
 # network cli
@@ -23,7 +23,7 @@ echo "alias tailf='tail -f'" >> /etc/bash.bashrc
 echo "set nu" >> /etc/vim/vimrc
 # java
 sudo apt-get install -y openjdk-17-jdk
-wget https://github.com/alibaba/arthas/releases/download/arthas-all-3.6.6/arthas-bin.zip
+wget https://github.com/alibaba/arthas/releases/download/arthas-all-3.6.7/arthas-bin.zip
 mkdir -p /usr/local/lib/arthas
 unzip arthas-bin.zip -d /usr/local/lib/arthas
 rm -rf arthas-bin.zip
