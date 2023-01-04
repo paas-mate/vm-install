6f3336a61478da4583d88a90ba966533e4a61ac5
goflutterjava <goflutterjava@gmail.com>
2023-01-04 10:20:17 +0800

    update base script

--- ubuntu/base.sh
+++ ubuntu/base.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
-# c language
-sudo apt-get install -y cmake
-sudo apt-get install -y clang
-# ebpf
-sudo apt-get install -y linux-tools-$(uname -r) linux-headers-$(uname -r)
-# sudo apt-get install -y perf-tools-unstable
-sudo apt-get install -y libbpfcc
-sudo apt-get install -y bpftrace
 # network cli
 sudo apt-get install -y bridge-utils arping net-tools
 sudo apt-get install -y ipvsadm
 sudo apt-get install -y iperf3
 sudo apt-get install -y qperf
-# openvswitch
-sudo apt-get install -y openvswitch-switch
-# helpful command
-echo "alias ..='cd ..'" >> /etc/bash.bashrc
-echo "alias ...='cd ../..'" >> /etc/bash.bashrc
-echo "alias tailf='tail -f'" >> /etc/bash.bashrc
-echo "set nu" >> /etc/vim/vimrc
+# c language
+sudo apt-get install -y cmake
+sudo apt-get install -y clang
 # java
 sudo apt-get install -y openjdk-17-jdk
 wget https://github.com/alibaba/arthas/releases/download/arthas-all-3.6.7/arthas-bin.zip
@@ -28,3 +16,17 @@ mkdir -p /usr/local/lib/arthas
 unzip arthas-bin.zip -d /usr/local/lib/arthas
 rm -rf arthas-bin.zip
 echo "alias arthas='java -jar /usr/local/lib/arthas/arthas-boot.jar'" >> /etc/bash.bashrc
+# ebpf
+sudo apt-get install -y linux-tools-$(uname -r) linux-headers-$(uname -r)
+# sudo apt-get install -y perf-tools-unstable
+sudo apt-get install -y libbpfcc
+sudo apt-get install -y bpftrace
+# xdp
+sudo apt-get install -y clang llvm libelf-dev libpcap-dev gcc-multilib build-essential
+# openvswitch
+sudo apt-get install -y openvswitch-switch
+# helpful command
+echo "alias ..='cd ..'" >> /etc/bash.bashrc
+echo "alias ...='cd ../..'" >> /etc/bash.bashrc
+echo "alias tailf='tail -f'" >> /etc/bash.bashrc
+echo "set nu" >> /etc/vim/vimrc
